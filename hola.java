906855b6b4a5f1172bd44c8dd514dc28b57e3ed3
SerranoSimon <sserrano2024@inf.udec.cl>
2025-04-23 22:07:15 -0600

    u2

--- hola.java
+++ hola.java
@@ -3,5 +3,6 @@ public class hola {
         System.out.println("hoka3rrr33");
         System.out.println("hola3ee33");
         System.out.println("EeeEe");
+        System.out.println("dw");
     }
 }
